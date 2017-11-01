838d9cd0de2dfb222c1c4edcdf5fa82773ce150e
René van den Berg <rjwvandenberg@gmail.com>
2017-11-01 11:43:02 +0100

    Officer_module: Ignore any message without member variable

--- bot_modules/officer_module.js
+++ bot_modules/officer_module.js
@@ -9,7 +9,8 @@ const prefix = config.prefix;
 let officerRoles = ["Leader", "Officer", "ADMIN"];
 
 exports.run = (client, message, args) => {
-	if(message.member && !message.member.roles.some(r=>officerRoles.includes(r.name))) return;
+	if(!message.member) return;
+	if(!message.member.roles.some(r=>officerRoles.includes(r.name))) return;
 	if(message.author.bot) return;
 
 	if(message.content.startsWith(prefix + "purge")) {
